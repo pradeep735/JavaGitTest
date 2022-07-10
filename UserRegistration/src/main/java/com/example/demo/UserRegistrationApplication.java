45d2d251a8840427888de399af4de497f9355f10
pradeep735 <81068219+pradeep735@users.noreply.github.com>
2022-07-10 11:56:06 +0530

    Update UserRegistrationApplication.java

--- UserRegistration/src/main/java/com/example/demo/UserRegistrationApplication.java
+++ UserRegistration/src/main/java/com/example/demo/UserRegistrationApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class UserRegistrationApplication {
-
+//this is from portal
 	public static void main(String[] args) {
 		SpringApplication.run(UserRegistrationApplication.class, args);
 	}
