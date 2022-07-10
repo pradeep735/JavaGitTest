7c04afd17575828aa940875f49198b8b31e5a085
pradeep735 <spradeepk735@gmail.com>
2022-07-10 11:50:37 +0530

    test commit in jira1234

--- UserRegistration/src/main/java/com/example/demo/UserRegistrationApplication.java
+++ UserRegistration/src/main/java/com/example/demo/UserRegistrationApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class UserRegistrationApplication {
-
+	//this is test
 	public static void main(String[] args) {
 		SpringApplication.run(UserRegistrationApplication.class, args);
 	}
